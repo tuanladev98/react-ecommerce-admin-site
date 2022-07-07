d5909357781777b7ee2357cabbe1f3d163c521b0
Le Anh Tuan <leanhtuan.th98@gmail.com>
2022-07-07 23:14:42 +0700

    update list newest transactions

--- src/components/widget-lg/WidgetLg.jsx
+++ src/components/widget-lg/WidgetLg.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { format as formatTimeAgo } from 'timeago.js';
+import { Visibility } from '@material-ui/icons';
 
 import './WidgetLg.css';
 
@@ -32,6 +34,7 @@ export default function WidgetLg() {
           <th className="widgetLgTh">Date</th>
           <th className="widgetLgTh">Amount</th>
           <th className="widgetLgTh">Status</th>
+          <th className="widgetLgTh"></th>
         </tr>
         {newestTransactions.map((transaction) => {
           return (
@@ -44,61 +47,24 @@ export default function WidgetLg() {
                 />
                 <span className="widgetLgName">{transaction.user.name}</span>
               </td>
-              <td className="widgetLgDate">{transaction.createdAt}</td>
+              <td className="widgetLgDate">
+                {formatTimeAgo(transaction.createdAt)}
+              </td>
               <td className="widgetLgAmount">
                 {numberWithCommas(transaction.amount)}₫
               </td>
               <td className="widgetLgStatus">
                 <Button type="Approved" />
               </td>
+              <td className="widgetLgDetail">
+                <button className="widgetLgDetailBtn">
+                  <Visibility className="widgetLgDetailIcon" />
+                  Details
+                </button>
+              </td>
             </tr>
           );
         })}
-        {/* <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr> */}
       </table>
     </div>
   );

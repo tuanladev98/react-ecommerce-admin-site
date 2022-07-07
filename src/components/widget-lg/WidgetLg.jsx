7f98b3742f25c5aaa12fe09fd4e842162b8e10af
Le Anh Tuan <tuanla1@hybrid-technologies.nv>
2022-07-07 14:41:49 +0700

    fetching newest transaction data (pending handle button order status)

--- src/components/widget-lg/WidgetLg.jsx
+++ src/components/widget-lg/WidgetLg.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+
 import './WidgetLg.css';
 
+import statsApis from '../../api/stats.api';
+import numberWithCommas from '../../utils/numberWithCommas';
+
 export default function WidgetLg() {
+  const [newestTransactions, setNewestTransactions] = useState([]);
+
+  useEffect(() => {
+    statsApis
+      .getListNewestTransaction()
+      .then((result) => {
+        setNewestTransactions(result.data);
+      })
+      .catch((error) => {
+        setNewestTransactions([]);
+      });
+  }, []);
+
   const Button = ({ type }) => {
     return <button className={'widgetLgButton ' + type}>{type}</button>;
   };
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Newest transactions</h3>
@@ -14,21 +33,27 @@ export default function WidgetLg() {
           <th className="widgetLgTh">Amount</th>
           <th className="widgetLgTh">Status</th>
         </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
+        {newestTransactions.map((transaction) => {
+          return (
+            <tr className="widgetLgTr" key={transaction.id}>
+              <td className="widgetLgUser">
+                <img
+                  src="https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                  alt=""
+                  className="widgetLgImg"
+                />
+                <span className="widgetLgName">{transaction.user.name}</span>
+              </td>
+              <td className="widgetLgDate">{transaction.createdAt}</td>
+              <td className="widgetLgAmount">
+                {numberWithCommas(transaction.amount)}
+              </td>
+              <td className="widgetLgStatus">
+                <Button type="Approved" />
+              </td>
+            </tr>
+          );
+        })}
         {/* <tr className="widgetLgTr">
           <td className="widgetLgUser">
             <img
